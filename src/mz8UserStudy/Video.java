2429ce93c1c2ae5c20fb008fbbf3936b21adbd80
Tanja <tanja.matura@hotmail.com>
2015-12-08 10:41:25 +0100

    Main Klasse

--- src/mz8UserStudy/Video.java
+++ src/mz8UserStudy/Video.java
@@ -51,6 +51,7 @@ public class Video {
 	public String getURL(){ return url; }
 	public String getName(){ return name; }
 	public int getBewertung(int wert){ return bewertungen[wert]; }
+	public int getAnzahlBewertungen(){ return anzahlBewertungen; }
 	
 	public void addBewertung(int wert){ bewertungen[wert]++; anzahlBewertungen++;}
 	
@@ -67,29 +68,5 @@ public class Video {
 	// Durchschnittliche Bewertung 
 	public double getDurchschnittlicheBewertung(){		
 		return sumBewertungen()/anzahlBewertungen; 
-	}
-
-	
-	// Main hab ich nur inzwischen gemmacht, um die Methoden zu testen ^^
-	public static void main(String[] args) { 		
-		Video meinVideo = new Video("url", "name"); 
-		meinVideo.addBewertung(3); 
-		meinVideo.addBewertung(2);
-		meinVideo.addBewertung(3);
-		meinVideo.addBewertung(10);
-		System.out.println("Anzahl der Bewertungen: " + meinVideo.anzahlBewertungen + 
-							"\nDurchschnittliche Bewertung: " + meinVideo.getDurchschnittlicheBewertung() + 
-							"\n \nAufschlüsselung:" + 
-							"\n 0: " + meinVideo.getBewertung(0) +
-							"\n 1: " + meinVideo.getBewertung(1) +
-							"\n 2: " + meinVideo.getBewertung(2) +
-							"\n 3: " + meinVideo.getBewertung(3) +
-							"\n 4: " + meinVideo.getBewertung(4) +
-							"\n 5: " + meinVideo.getBewertung(5) +
-							"\n 6: " + meinVideo.getBewertung(6) +
-							"\n 7: " + meinVideo.getBewertung(7) +
-							"\n 8: " + meinVideo.getBewertung(8) +
-							"\n 9: " + meinVideo.getBewertung(9) +
-							"\n 10: " + meinVideo.getBewertung(10));
 	}
 }

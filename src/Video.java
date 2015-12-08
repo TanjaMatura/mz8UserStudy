9447c5f9fd7316884475483e550ed6daeb73803e
Tanja <tanja.matura@hotmail.com>
2015-12-08 15:09:37 +0100

    Video update

--- src/Video.java
+++ src/Video.java
@@ -1,10 +1,7 @@
 
 import java.util.ArrayList;
 
-/* Ich hab mal eine Klasse angefangen, die Variablen sind aber noch lange nicht vollständig 
-*   Wir müssen uns eh überlegen, welchen Dateityp wir für Welche Antwortmöglichkeit am besten nehmen. 
-*   Ich hab mir überlegt: 
-*   	- Ja/Nein: Array mit zwei Plätzen, die zählen wie oft was jeweils ausgewählt wurde
+/*      - Ja/Nein: Array mit zwei Plätzen, die zählen wie oft was jeweils ausgewählt wurde
 *   	- Abstufung: Auch Arrays mit 5 bzw 10 Plätzen, je nachdem wie viele Antwortmöglichkeiten wir haben.
 *   				 Den Mittelwert usw können wir uns dann direkt aus dem Array berechnen, wir haben auch 
 *   				 automatisch gespeichert, wie viele Antworten schon gegeben wurden, welche wie oft usw. 
@@ -14,27 +11,33 @@ public class Video {
 	private String url; 
 	private String name; 
 	private int anzahlBewertungen; 
+	private int angeboten; 
+	private int ausgewaehlt; 
 	
-	private int[] beispielJaNein = new int[3];  	// beispielJaNein[0] = Ja, beispielJaNein[1] = Nein, beispielJaNein[2] = NA
-	private int[] beispielAbstufung = new int[6]; 
+	// Ja/Nein (NA)
+	private int[] schongesehen = new int[3]; 
+	private int[] plottwist = new int[3]; 
+	private int[] catchphrase = new int[3]; 
+	private int[] gerngesehen = new int[3];  
+	private int[] ueberzeugung = new int[3]; 
+	private int[] aufmerksamkeit = new int[3]; 
+	private int[] markebekannt = new int[3]; 
+	
+	// Abstufung (kein NA, da es Mittelstufe = Neutral gibt)
+	private int[] produktfixierung = new int[5]; 
+	private int[] lachenweinen = new int[5]; 
+	
+	private int[] froehlichtraurig = new int[5]; 
+	private int[] lustigernst = new int[5]; 
+	private int[] altmodischmodern = new int[5]; 
+	private int[] kreativunkreativ = new int[5]; 
+	private int[] liebenswertfies = new int[5]; 
+	private int[] sympathischunsympathisch = new int[5]; 
+	
+	private int[] zielgruppe = new int[7]; 
+	
 	private int[] bewertungen = new int[11];
 	
-	// Falls wir die wirklich auch erheben wollen, brauchen wir eigentlich 
-	// auch eine Objektklasse für denjenigen, der die Fragen beantwortet, oder?
-	// Vll auch nicht für jeden, sondern quasi eine für jedes geschlecht wo die antowrten 
-	// dann gesammelt werden? Ich weiß grad nicht, gibts ws eine gscheitere Möglichkeit ^^ 
-	// private int[] geschlecht = new int[3]; 			// weiblich, männlich, was anderes? Also drei Möglichkeiten?
-	// private int[] alter = new int[5];				// Einfach mal 5 Altersgruppen als Vorschlag
-	
-	/*
-	 *  Andere Möglichkeit wie wirs auch machen können: 
-	 *  Für die Bewertungen eine ArrayList nehmen und jede Bewertung hinten drann hängen. 
-	 *  Kommt mir persönlich aber umständlicher vor, vor allem, wenn wir dann wissen wollen, 
-	 *  was wie oft gegeben wurde, oder? 
-	 *  Denke wir solltens so wie ichs jetzt mal hab machen, da wir ja eh eine bestimmte Anzahl an 
-	 *  Auswahlmöglichkeiten haben. 
-	 */
-	//public ArrayList bewertungen; 					
 	
 	/* ------------ Konstruktor  ------------ */
 	
@@ -42,23 +45,43 @@ public class Video {
 		this.url = url; 
 		this.name = name; 
 		anzahlBewertungen = 0; 
-		//bewertungen = new ArrayList<Integer>(); 
+		angeboten = 0; 
+		ausgewaehlt = 0; 
 	}
 
 	/* ------------ Getter ------------ */
 	
 	public String getURL(){ return url; }
 	public String getName(){ return name; }
+	
+	public int getSchonGesehen(int wert){ return schongesehen[wert]; }
+	public int getPlotTwist(int wert){ return plottwist[wert]; }
+	public int getCatchPhrase(int wert){ return catchphrase[wert]; }
+	public int getGernGesehen(int wert){ return gerngesehen[wert]; }
+	public int getUeberzeugung(int wert){ return ueberzeugung[wert]; }
+	public int getAufmerksamkeit(int wert){ return aufmerksamkeit[wert]; }
+	public int getMarkeBekannt(int wert){ return markebekannt[wert]; }
+	
+	public int getProduktfixierung(int wert){ return produktfixierung[wert]; }
+	public int getLachenWeinen(int wert){ return lachenweinen[wert]; }
+	
+	public int getFroehlichTraurig(int wert){ return froehlichtraurig[wert]; }
+	public int getLustigErnst(int wert){ return lustigernst[wert]; }
+	public int getAltmodischModern(int wert){ return altmodischmodern[wert]; }
+	public int getKreativUnkreativ(int wert){ return kreativunkreativ[wert]; }
+	public int getLiebenswertFies(int wert){ return liebenswertfies[wert]; }
+	public int getSympathischUnsympathisch(int wert){ return sympathischunsympathisch[wert]; }
+	
+	public int getZielgruppe(int wert){ return zielgruppe[wert]; }
+	
 	public int getBewertung(int wert){ return bewertungen[wert]; }
 	public int getAnzahlBewertungen(){ return anzahlBewertungen; }
-	
-	public void addBewertung(int wert){ bewertungen[wert]++; anzahlBewertungen++;}
-	
+	public double getPickRatio(){ return angeboten/ausgewaehlt; }
 	
 	/* ------------ Komplexere Methoden ------------ */
 	
 	// Summe der Bewertungen (nicht Anzahl) 
-	public double sumBewertungen(){		
+	public double getSumBewertungen(){		
 		int sBewertung = 0;
 		for (int i = 0; i < bewertungen.length; i++) { sBewertung += bewertungen[i]*i; }
 		return sBewertung; 
@@ -66,6 +89,9 @@ public class Video {
 	
 	// Durchschnittliche Bewertung 
 	public double getDurchschnittlicheBewertung(){		
-		return sumBewertungen()/anzahlBewertungen; 
+		return getSumBewertungen()/anzahlBewertungen; 
 	}
+	
+	// Neue Bewertung hinzufuegen
+	public void addBewertung(int wert){ bewertungen[wert]++; anzahlBewertungen++;}
 }
